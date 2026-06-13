0c6bd0637aa3c1a9cd80855d44dfba0919fd8d45
Josué Bayidikila <josue-dev@outlook.fr>
2026-06-13 22:02:46 +0200

    feat: implement in-memory crud

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -48,3 +48,65 @@ async def study_sessions(session: CreateStudySession):
         "message": "Study session created",
         "session": session
     }
+
+goals = []
+
+
+@app.get('/goals')
+async def list_goals():
+    return goals
+
+
+class CreateGoal(BaseModel):
+    title: str
+    category: str
+
+
+@app.get('/goals/{goal_id}')
+async def get_goal(goal_id: int):
+    for goal in goals:
+        if goal['id'] == goal_id:
+            return goal
+
+    raise HTTPException(status_code=404, detail="Goal not found")
+
+
+@app.post('/goals')
+async def create_goal(goal: CreateGoal):
+    new_goal = {
+        "id": len(goals) + 1,
+        "title": goal.title,
+        "category": goal.category,
+        "is_completed": False
+    }
+
+    goals.append(new_goal)
+
+    return new_goal
+
+
+@app.put('/goals/{goal_id}')
+async def update_goal(goal_id: int, updated_goal: CreateGoal):
+
+    for goal in goals:
+        if goal['id'] == goal_id:
+            goal['title'] = updated_goal.title
+            goal['category'] = updated_goal.category
+
+            return goal
+
+    raise HTTPException(status_code=404, detail="Goal not found")
+
+
+@app.delete('/goals/{goal_id}')
+async def delete_goal(goal_id: int):
+    for goal in goals:
+        if goal['id'] == goal_id:
+            goals.remove(goal)
+
+            return {
+                "message": "Goal deleted",
+                "goal_id": goal_id
+            }
+
+    raise HTTPException(status_code=404, detail="Goal not found")
